c1272d19a6a14e37a1431f589335d60b4ed5a91b
Robert Tierie <70277787+roberttierie@users.noreply.github.com>
2025-11-10 11:12:18 +0800

    add CORS allowlist

--- src/index.js
+++ src/index.js
@@ -7,6 +7,25 @@ const webhookHandler = require('./handlers/webhook');
 
 const app = express();
 
+// CORS: only allow your store domains to call this API
+const ALLOW = new Set([
+  'https://mcduffy.co',
+  'https://www.mcduffy.co',
+  'https://mcduffytemporary.myshopify.com'
+]);
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin && ALLOW.has(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Vary', 'Origin');
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 // JSON for normal routes
 app.use('/api', express.json());
 
@@ -24,3 +43,4 @@ app.get('/healthz', (req, res) => res.send('ok'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on :${PORT}`));
+
